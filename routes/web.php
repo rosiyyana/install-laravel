a6806ae3b4166ca8e71eb2e6ebaf604c125819cf
unknown <rosiyana949@gmail.com>
2020-07-25 18:02:06 +0700

    tugas-web-laravel-routing-dan-view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/portofolio', function () {
+    return view('portofolio');
+});
+
+Route::get('/experience', function () {
+    return view('experience');
 });
